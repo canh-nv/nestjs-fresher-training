b3b7f031f8c0ca216e0c15730423360c1e30ebfa
Duong-N <maingocduongwork@gmail.com>
2024-12-23 22:06:16 +0700

    chore(ftb-4):Update file path

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -3,13 +3,13 @@ import { ProductService } from './product.service';
 import { ProductController } from './product.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Product } from './entities/product.entity';
-import { Category } from 'src/category/entities/category.entity';
+import { Category } from '../category/entities/category.entity';
 
 
 @Module({
-  imports:[TypeOrmModule.forFeature([Product,Category])],
+  imports: [TypeOrmModule.forFeature([Product, Category])],
   controllers: [ProductController],
   providers: [ProductService],
-  exports:[ProductService],
+  exports: [ProductService],
 })
-export class ProductModule {}
+export class ProductModule { }
