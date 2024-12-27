cb5c8d8f53218907a03d31fe731f225ea7760609
Duong-N <maingocduongwork@gmail.com>
2024-12-27 20:27:46 +0700

    test(ftb-4):setup category controller test

---
+++ src/category/category.controller.spec.ts
@@ -0,0 +1,37 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { CategoryController } from './category.controller';
+import { CategoryService } from './category.service';
+//import { CreateCategoryDto } from './dto/create-category.dto';
+//import { UpdateCategoryDto } from './dto/update-category.dto';
+
+describe('CategoryController', () => {
+    let controller: CategoryController;
+    //let sservice: CategoryService;
+
+    const mockCategoryService = {
+        create: jest.fn(),
+        findAll: jest.fn(),
+        findOne: jest.fn(),
+        update: jest.fn(),
+        remove: jest.fn(),
+    };
+
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            controllers: [CategoryController],
+            providers: [
+                {
+                    provide: CategoryService,
+                    useValue: mockCategoryService,
+                },
+            ],
+        }).compile();
+
+        controller = module.get<CategoryController>(CategoryController);
+        //sservice = module.get<CategoryService>(CategoryService);
+    });
+
+    it('should be defined', () => {
+        expect(controller).toBeDefined();
+    });
+});
